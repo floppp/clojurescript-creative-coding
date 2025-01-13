dea9e1eb688830f6565576e53c390a075cf1c18e
fernando <fernandolaso@duck.com>
2025-01-13 22:06:32 +0100

    Bug in line drawing after splitting space but nice
    It looks lke some random stars

--- src/main/qoback/particles_connection_canvas.cljs
+++ src/main/qoback/particles_connection_canvas.cljs
@@ -2,19 +2,21 @@
   (:require [p5]
             [core.canvas :as c]))
 
+(def width 1000)
+(def height 600)
 (def canvas (.getElementById js/document "whiteboard"))
 (def ctx (.getContext canvas "2d"))
-(set! (.-height canvas) 600)
-(set! (.-width canvas) 900)
+(set! (.-height canvas) height)
+(set! (.-width canvas) width)
 (set! (.. canvas -style -backgroundColor) "black")
-
+(def squares #js [])
 (def mass #js [])
 (def density 10)
 (def ps #js [])
 (def limit 100)
 (def dt 0.1)
 (def frame-count (atom nil))
-(def n-particles 50)
+(def n-particles 2000)
 
 (defn make-particle
   ([idx] (make-particle idx true))
@@ -22,8 +24,9 @@
    #js {:id idx
         :diameter 8
         :radius 4
-        :pos #js {:x (js/random 50 850)
-                  :y (js/random 50 550)}
+        :square nil
+        :pos #js {:x (js/random (/ limit 10) (- width (/ limit 10)))
+                  :y (js/random (/ limit 10) (- height (/ limit 10)))}
         :vel (if moving-particle?
                #js {:x (js/random -10 10)
                     :y (js/random -10 10)}
@@ -48,9 +51,56 @@
   (aset mass 1 nil)
   (reset! frame-count nil))
 
+(defn get-neighbors-idx
+  [^js p]
+  (let [sq (.-square p)
+        n-rows (/ height limit)
+        n-cols (/ width limit)
+        row (.-row sq)
+        col (.-col sq)
+        prev-row (if (zero? row) row (dec row))
+        next-row (if (= row (dec n-rows)) row (inc row))
+        prev-col (if (zero? col) col (dec col))
+        next-col (if (= col (dec n-cols)) col (inc col))]
+    (for [r (range prev-row (inc next-row))
+          c (range prev-col (inc next-col))]
+      (let [idx (+ c (* n-cols r))
+            neighbor-p (aget squares idx)]
+        neighbor-p))))
+
+(defn split-space [& [draw?]]
+  (set! (.-length squares) 0)
+  (let [w (/ limit 1)]
+    (doseq [idx (range (count ps))]
+      (let [^js p (aget ps idx)
+            n-cols (/ width w)
+            row (Math/floor (/ (.. p -pos -y) w))
+            col (Math/floor (/ (.. p -pos -x) w))
+            square (+ (* row n-cols) col)
+            current (aget squares square)]
+        (set! (.-square p) #js {:row row :col col})
+        (if (nil? current)
+          (aset squares square #js [idx])
+          (.push (aget squares square) idx))))
+    (when draw?
+      (doseq [yinit (range 0 height w)
+              xinit (range 0 width w)]
+        (let [x-idx (/ xinit w)
+              y-idx (/ yinit w)
+              square-idx (+ x-idx (* y-idx (/ width w)))]
+          (.beginPath ctx)
+          (.rect ctx xinit yinit w w)
+          (if (nil? (aget squares square-idx))
+            (set! (.-strokeStyle ctx) "rgba(255, 255, 255, 0.2")
+            (set! (.-strokeStyle ctx) "red"))
+          (.stroke ctx))))))
+
 (defn setup []
   (doseq [idx (range n-particles)]
     (aset ps idx (make-particle idx true)))
+  #_(doseq [^js p ps]
+      (let [ns (remove nil? (get-neighbors-idx p))]
+        (.log js/console ns)))
   (.removeEventListener canvas "mouseup" clean-mass)
   (.removeEventListener canvas "touchend" clean-mass)
   (.removeEventListener canvas "mousedown" set-mass) ;; click no sirve porque se dispara después que `touchend`.
@@ -74,14 +124,14 @@
       (when (< (.-x (.-pos p)) (.-radius p))
         (set! (.-x (.-pos p))  (.-radius p))
         (set! (.-x (.-vel p)) (- (.-x (.-vel p)))))
-      (when (> (.-x (.-pos p)) (- 900 (.-radius p)))
-        (set! (.-x (.-pos p)) (- 900 (.-radius p)))
+      (when (> (.-x (.-pos p)) (- width (.-radius p)))
+        (set! (.-x (.-pos p)) (- width (.-radius p)))
         (set! (.-x (.-vel p)) (- (.-x (.-vel p)))))
       (when (< (.-y (.-pos p)) (.-radius p))
         (set! (.-y (.-pos p)) (.-radius p))
         (set! (.-y (.-vel p)) (- (.-y (.-vel p)))))
-      (when (> (.-y (.-pos p)) (- 600 (.-radius p)))
-        (set! (.-y (.-pos p)) (- 600 (.-radius p)))
+      (when (> (.-y (.-pos p)) (- height (.-radius p)))
+        (set! (.-y (.-pos p)) (- height (.-radius p)))
         (set! (.-y (.-vel p)) (- (.-y (.-vel p))))))))
 
 (defn update-vs []
@@ -118,6 +168,41 @@
     (let [^js p (nth ps i)
           ^js q (nth ps j)
           d (distance p q)]
+      ;; (get-neighbors p)
+      (when (< d limit)
+        (let [opacity (/ (- limit d) limit)
+              p-pos (.. p -pos)
+              q-pos (.. q -pos)]
+          (c/draw-line
+           ctx
+           (.-x p-pos) (.-y p-pos)
+           (.-x q-pos) (.-y q-pos)
+           {:c (str "rgba(255, 255, 255, " opacity ")")}))))))
+
+(defn draw-neighbors-lines
+  "Avoding redundant drawns."
+  []
+  (doseq [^js p ps]
+    (let [ns (remove nil? (get-neighbors-idx p))]
+      (doseq [^js n ns]
+        (let [q (aget ps (first n))
+              d (distance p q)]
+          (when (< d limit)
+            (let [opacity (/ (- limit d) limit)
+                  p-pos (.. p -pos)
+                  q-pos (.. q -pos)]
+              (c/draw-line
+               ctx
+               (.-x p-pos) (.-y p-pos)
+               (.-x q-pos) (.-y q-pos)
+               {:c (str "rgba(255, 255, 255, " opacity ")")})))))))
+
+  #_(doseq [i (range n-particles)
+          j (range (inc i) n-particles)]
+    (let [^js p (nth ps i)
+          ^js q (nth ps j)
+          d (distance p q)]
+      ;; (get-neighbors p)
       (when (< d limit)
         (let [opacity (/ (- limit d) limit)
               p-pos (.. p -pos)
@@ -177,46 +262,13 @@
                          {:c (str "rgba(255, 255, 255, " acc-m ")")
                           ;; :w (if (> acc-m 10) 20 1)
                           })))))))
-
-(def squares #js [])
-
 (defn mouse-released [] (clean-mass))
-(def n-squares (atom 0))
-
-(defn split-space []
-  (set! (.-length squares) 0)
-  (reset! n-squares 0)
-  (let [w (/ limit 2)]
-    (doseq [idx (range (count ps))]
-      (let [p (aget ps idx)
-            n-rows (/ 600 w)
-            n-cols (/ 900 w)
-            row (Math/floor (/ (.. p -pos -y) w))
-            col (Math/floor (/ (.. p -pos -x) w))
-            square-idx (+ (* row n-cols) col)
-            current (aget squares square-idx)]
-        (if (nil? current)
-          (aset squares square-idx #js [idx])
-          (.push (aget squares square-idx) idx))))
-    (doseq [yinit (range 0 600 w)
-            xinit (range 0 900 w)]
-      (swap! n-squares #(inc %))
-      (let [x-idx (/ xinit w)
-            y-idx (/ yinit w)
-            square-idx (+ x-idx (* y-idx (/ 900 w)))]
-        ;; (.log js/console square-idx)
-        (.beginPath ctx)
-        (.rect ctx xinit yinit w w)
-        ;; (set! (.-strokeStyle ctx) "white")
-        (if (nil? (aget squares square-idx))
-          (set! (.-strokeStyle ctx) "gray")
-          (set! (.-strokeStyle ctx) "red"))
-        (.stroke ctx)))))
 
 (defn draw []
-  (. ctx (clearRect 0 0 900 600))
+  (. ctx (clearRect 0 0 width height))
   (split-space)
-  (draw-lines)
+  ;; (draw-lines)
+  (draw-neighbors-lines)
   (doseq [^js p ps]
     (c/draw-circle ctx (.. p -pos -x) (.. p -pos -y) (.-radius p) "blue"))
   ;; tiene que estar antes de actualizar
